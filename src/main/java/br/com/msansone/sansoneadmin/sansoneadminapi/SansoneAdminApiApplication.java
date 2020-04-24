8d7902da78767eb669852949068b79a53e745453
marcio sansone <msansone@gmail.com>
2020-04-24 11:17:26 +0100

    first commit, just create project with spring boot initialize

---
+++ src/main/java/br/com/msansone/sansoneadmin/sansoneadminapi/SansoneAdminApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.sansoneadmin.sansoneadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SansoneAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SansoneAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/msansone/sansoneadmin/sansoneadminapi/SansoneAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msansone.sansoneadmin.sansoneadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansoneAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
