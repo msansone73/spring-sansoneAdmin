d95475e73a12171cdc3ab251d00ae2a93b971dd8
marcio sansone <msansone@gmail.com>
2020-08-19 19:55:50 +0100

    added Empresa services

--- src/main/java/br/com/msansone/sansoneadmin/sansoneadminapi/controller/EmpresaController.java
+++ src/main/java/br/com/msansone/sansoneadmin/sansoneadminapi/controller/EmpresaController.java
@@ -6,6 +6,10 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,16 +32,36 @@ public class EmpresaController {
 	@Autowired
 	PerfilService perfilService;
 
-	@GetMapping("empresa")
+	@GetMapping("/empresa")
 	public List<Empresa> listEmpresas() {
 		LOG.info("GET - /sansoneadmin/empresa");
 		return empresaService.getAll();
 	}
 	
-	@GetMapping("perfil")
+	@GetMapping("/empresa/{id}")
+	public Empresa listEmpresas(@PathVariable Long id) {
+		LOG.info("GET - /sansoneadmin/empresa/{id}");
+		return empresaService.getById(id);
+	}
+	
+	@GetMapping("/perfil")
 	public List<Perfil> listPerfis() {
 		LOG.info("GET - /sansoneadmin/perfil");
 		return perfilService.getAll();
+	}
+	
+	@PostMapping("/empresa")
+	public Empresa addEmpresa(@RequestBody Empresa empresa) {
+		LOG.info("POST - /sansoneadmin/empresa");
+		return empresaService.addEmpresa(empresa);		
+	}
+	
+	@PutMapping("/empresa/{id}")
+	public Empresa updateEmpresa(@PathVariable("id") long id, 
+            @RequestBody Empresa empresa) {
+		LOG.info("PUT - /sansoneadmin/empresa/{id}");
+		
+		return empresaService.updtEmpresa(id, empresa);
 	}
 
 }
--- src/main/java/br/com/msansone/sansoneadmin/sansoneadminapi/service/EmpresaService.java
+++ src/main/java/br/com/msansone/sansoneadmin/sansoneadminapi/service/EmpresaService.java
@@ -1,6 +1,7 @@
 package br.com.msansone.sansoneadmin.sansoneadminapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,29 @@ public class EmpresaService {
 	
 	public List<Empresa> getAll() {
 		return empresaRepository.findAll();
+	}
+
+	public Empresa addEmpresa(Empresa empresa) {
+		return empresaRepository.save(empresa);
+	}
+
+	public Empresa getById(Long id) {
+		Optional<Empresa> empresa = empresaRepository.findById(id);
+		return empresa.isPresent()?empresa.get():null;
+	}
+
+	public Empresa updtEmpresa(long id, Empresa empresa) {
+		
+		Empresa empresaDb = getById(id);
+		
+		if (empresaDb==null) {
+			return null;
+		} else {
+			empresaDb.setNome(empresa.getNome());
+			empresaDb.setDescricao(empresa.getDescricao());
+			return empresaRepository.save(empresaDb);
+		}
+		
 	}
 
 }
